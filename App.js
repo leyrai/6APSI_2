015faa394f89edd9218c03f477fd28100f3f67bb
leyrai <rgtiglao@student.hau.edu.ph>
2025-06-24 15:02:07 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './landingpage';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ LoginPage.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './style.css'; // Import the CSS file
+
+function LoginPage() {
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        className="login-input"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="login-input"
+      />
+      <button className="login-button">Login</button>
+    </div>
+  );
+}
+
+export default LoginPage;
